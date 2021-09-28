5055fd385955d9c2dbab8272d1657f200da496ec
2theMoon <dlaguddh1@gmail.com>
2021-09-28 15:59:26 +0900

    edit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -82,6 +82,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack)
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -149,12 +156,12 @@ function makeConnection() {
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
-function handleIce(data){
+function handleIce(data) {
   console.log("send candidate");
   socket.emit("ice", data.candidate, roomName);
 }
 
-function handleAddStream(data){
+function handleAddStream(data) {
   const peerFace = document.getElementById("peerFace");
   peerFace.srcObject = data.stream;
 }
